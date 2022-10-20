5e62f7ab2c024b4d0926df086a86afe1965f77b8
Diego Fernando Siqueira Silva <diegofernando.ssiqueira@gmail.com>
2022-10-20 19:23:38 -0300

    test view controller

--- MeuDinheiro/Mercado/Controller/MoedasViewController.swift
+++ MeuDinheiro/Mercado/Controller/MoedasViewController.swift
@@ -1,29 +1,16 @@
-//
-//  MoedasViewController.swift
-//  MeuDinheiro
-//
-//  Created by Diego Fernando on 10/20/22.
-//
-
 import UIKit
 
 class MoedasViewController: UIViewController {
 
+    lazy var tableView: UITableView = {
+       let table = UITableView()
+        table.translatesAutoresizingMaskIntoConstraints = false
+//        table.register(MercadoTableViewCell.self, forCellReuseIdentifier: MercadoTableViewCell.identifier)
+        return table
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .green
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
